05dd18ee9d9d69f3d587cde6005df4d6d197e849
Ana <ana@mipc.com>
2019-08-07 20:31:42 +0200

    Comprobar si una palabra es palíndromo o no

---
+++ Devo_text.py
@@ -0,0 +1,39 @@
+test1 = 'AB'
+#test2 = 'yo soy'
+#test2= test2.rstrip()
+def reverse(word):
+    result = ''
+    long = len(word)
+    for char in word:
+        char = word[long - 1]
+        long = long - 1
+        result = result + char
+    return result
+
+def compare(new, original):
+        if new == original:
+            print('Word', original, 'is a palindromo')
+        else:
+            print('Not a palindromo')
+
+def ispalindrome(word):
+    palindrome = True
+    lenght = len(word)
+    half = int(lenght/2)
+    start = 0
+    while start < half:
+        if word[start] == word[lenght-1]:
+            start = start + 1
+            lenght = lenght - 1
+        else:
+            palindrome = False
+            break
+    return palindrome
+#Opción más rápida solo recorre hasta la mitad de la palabra
+print(ispalindrome(test1))
+
+#Opción 2 da la vuelta a la palabra, la guarda en una variable y la compara con la original
+print(reverse(test1))
+result = reverse(test1)
+compare(result, test1)
+ispalindrome(test1)
